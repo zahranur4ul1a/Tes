af5600f714c1f29f243c0729fc3eb5085dc8576b
zahra nur aulia <zahra_aulia_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:14:44 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,7 @@
+public class Tes {
+    public static void main(String[] args) {
+        String nama = "aya";
+        string kelas = "X RPL 5";
+        System.out.print("Halo, nama saya " + nama + " dari kelas " + kelas);
+    }
+}
